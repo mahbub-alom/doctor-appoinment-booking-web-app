ad42caab14b3d1475bde05dce7a8eff455479702
mahbub-alom <mdmahbuba034@gmail.com>
2024-10-17 14:20:52 +0600

    hero section done

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
-
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-
+      {/* hero section */}
+      <Hero />
     </div>
   );
 }
